11345be9ff7a0ea195918b901165be6bacd80cd7
SOUNDARYA39 <soundaryagpt.g2@gmail.com>
2023-07-12 12:53:40 +0530

    firstcommit

---
+++ src/test/java/sampletestScripts/SamplePrg.java
@@ -0,0 +1,19 @@
+package sampletestScripts;
+
+import org.testng.annotations.Test;
+
+public class SamplePrg {
+	
+	@Test
+	public void m1()
+	{
+		System.out.println("hello");
+	}
+	
+	@Test
+	public void m2() 
+	{
+		System.out.println("hi");
+	}
+
+}
